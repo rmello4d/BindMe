8e6f18791e33b82080d4b24a72eb6e15f1dc1a76
Ricardo Mello <ricardo.mello@4d.com>
2013-12-05 17:57:12 +0100

    Update designer.js

--- designer.js
+++ designer.js
@@ -7,18 +7,3 @@
 });
 
 // For more information, refer to http://doc.wakanda.org/Wakanda0.DevBranch/help/Title/en/page3870.html: 'BindMe value',
-        typeValue: 'string',
-        defaultValue: '' 
-    },{
-        name: 'data-binding-value',
-        description: 'BindMe with Source',
-        typeValue: 'datasource'
-    }]);
-
-    /*Default size*/
-    BindMe.setWidth('200');
-    BindMe.setHeight('20');
-
-})();
-
-// For more information, refer to http://doc.wakanda.org/Wakanda0.DevBranch/help/Title/en/page3870.html
